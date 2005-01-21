4df76045e51284bda1856f7041fe6f84ec554f8a
vstrelow <vstrelow>
2005-01-21 00:50:25 +0000

    SKT initial mit enums

---
+++ alricg/src/org/d3s/alricg/Prozessor/SKT.java
@@ -0,0 +1,20 @@
+/*
+ * Created on 21.01.2005 / 01:14:15
+ *
+ * This file is part of the project ALRICG. The file is copyright
+ * protected an under the GPL licence.
+ * For more information see "http://alricg.die3sphaere.de/".
+ *
+ */
+package org.d3s.alricg.Prozessor;
+
+/**
+ * <u>Beschreibung:</u><br> 
+ * Die Steigerungs-Kosten-Tabelle, nach der die Kosten für den Kauf von CharElementen bezahlt wird.
+ * @author V. Strelow
+ */
+public class SKT {
+    
+    public enum KostenKlasse { A_PLUS, A, B, C, D, E, F, G, H }
+    
+}
