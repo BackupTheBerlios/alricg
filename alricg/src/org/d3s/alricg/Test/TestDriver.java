ce89b6f3d84f55168c3827cdee45da6d8d6bb3ce
vstrelow <vstrelow>
2005-01-27 19:29:57 +0000

    Ein paar Fälle durchgespielt

--- alricg/src/org/d3s/alricg/Test/TestDriver.java
+++ alricg/src/org/d3s/alricg/Test/TestDriver.java
@@ -10,14 +10,23 @@ package org.d3s.alricg.Test;
 
 import java.io.File;
 
+import org.d3s.alricg.CharKomponenten.*;
+import org.d3s.alricg.CharKomponenten.CharZusatz.*;
+import org.d3s.alricg.CharKomponenten.Links.*;
+
 /**
  * <u>Beschreibung:</u><br> 
  * Diese Klasse dient den Testen von Alricg!
  * @author V. Strelow
  */
 public class TestDriver {
-
+	public enum testEnum { EINS, ZWEI, DREI }
+	
 	public static void main(String[] args) {
+		
+		TestDriver TD = new TestDriver();
+		/*
+		
 		File x = new File("lala.txt");
 		
 		try {
@@ -25,5 +34,63 @@ public class TestDriver {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		*/
+	}
+	
+	public TestDriver()  {
+		
+		testFile();
+		//testGeneric();
+		//testGeneric(new Integer(4));
+	}
+	
+	public void testFile() {
+		File f = new File("ressourcen" + File.separator + "lala.txt");
+		System.out.println(f.exists());
+		
+	}
+	
+	
+	public <T> void testGeneric( T m )
+	{
+		T blub;
+	  	
+	  	System.out.println(m.toString());
+	  	//return Math.random() > 0.5 ? m : n;
+	}
+	
+	public <T> void getTestGeneric()
+	{
+	    T;
+	  	
+	  	System.out.println(m.toString());
+	  	//return Math.random() > 0.5 ? m : n;
+	}
+	
+	
+	private void testWuerfel() {
+		WuerfelSammlung w = new WuerfelSammlung(5, null, new int[]{6});
+		
+		System.out.println(w.getWuerfelWurf());
+		System.out.println(w.getWuerfelWurf());
+		System.out.println(w.getWuerfelWurf());
+		System.out.println(w.getWuerfelWurf());
+		System.out.println(w.getWuerfelWurf());
+		System.out.println(w.getWuerfelWurf());
+		
+	}
+	
+	private void testEnum() {
+		/*int[] i = new int[] {0,1,2,3}; 
+		
+		
+		Eigenschaften e = Eigenschaften.MU;
+		
+		//System.out.println(i[Eigenschaften.MU]);
+		
+		switch (e) {
+		case Eigenschaften.MU: System.out.println(""); break;
+		default:
+		}*/
 	}
 }
