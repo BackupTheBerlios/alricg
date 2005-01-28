d8362de7e923bc8f1a1596df40a79451cf77b443
vstrelow <vstrelow>
2005-01-28 01:06:45 +0000

    Anfänge der Initialisierung

--- alricg/src/org/d3s/alricg/Controller/ProgAdmin.java
+++ alricg/src/org/d3s/alricg/Controller/ProgAdmin.java
@@ -11,12 +11,11 @@ import java.io.File;
 import java.io.IOException;
 
 import nu.xom.Builder;
+import nu.xom.Document;
+import nu.xom.Element;
 import nu.xom.ParsingException;
 
-import org.d3s.alricg.Test.TestDriver;
-import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.XMLReaderFactory;
+import org.d3s.alricg.CharKomponenten.Eigenschaften;
 
 /**
  * <b>Beschreibung:</b><br>
@@ -25,7 +24,8 @@ import org.xml.sax.helpers.XMLReaderFactory;
  * @stereotype singelton
  */
 public class ProgAdmin {
-	public static final String DATEI_CONFIG = "config.xml";
+	public static final String DATEI_CONFIG = "ressourcen" + File.separator 
+											+ "config.xml";
 	public static final String PFAD_XML_DATEN_D3S = "ressourcen" + File.separator 
 											+ "daten" + File.separator 
 											+ "basisDaten" + File.separator;
@@ -37,7 +37,7 @@ public class ProgAdmin {
 	public static final String PFAD_LIBRARY = "ressourcen" + File.separator 
 											+ "daten" + File.separator 
 											+ "text" + File.separator;
-	public static final String PFAD_Bilder = "img" + File.separator;
+	public static final String PFAD_BILDER = "img" + File.separator;
 	
 	// Werden durch config File eingelesen:
 	public static String library;
@@ -50,33 +50,40 @@ public class ProgAdmin {
 	}
 	
 	public ProgAdmin() {
-		buildXML();
+		Element configRoot, tmpElement;
+		
+		configRoot = getRootElement( new File(DATEI_CONFIG) );
+		
+		// Initialisiere die Library
+		tmpElement = getRootElement( new File( PFAD_LIBRARY 
+						+ configRoot.getFirstChildElement("library")
+						.getAttribute("file").getValue()) );
+		Library.initLibrary(tmpElement);
+		
+		// Lese die Komponenten ein
+		
+		
+		// TEST Wieder entfernen!
+		System.out.println(Library.getShortText(Eigenschaften.FF.getBezeichnung()));
+		System.out.println(Library.getShortText("AT"));
+		System.out.println(Library.getShortText("Körperkraft"));
+		
 	}
-	
+
 
 	
-	public void buildXML(){
-		 String url = DATEI_CONFIG;
-		  try {      
-		    XMLReader xerces = XMLReaderFactory.createXMLReader("org.apache.xerces.parsers.SAXParser"); 
-		    xerces.setFeature("http://apache.org/xml/features/validation/schema", true);                         
-
-		    Builder parser = new Builder(xerces, true);
-		    parser.build(DATEI_CONFIG);
-		    System.out.println(DATEI_CONFIG + " is schema valid.");
-		  }
-		  catch (SAXException ex) {
-		    System.out.println("Could not load Xerces.");
-		    System.out.println(ex.getMessage());
-		  }
-		  catch (ParsingException ex) {
-		    System.out.println(DATEI_CONFIG + " is not schema valid.");
-		    System.out.println(ex.getMessage());
-		    System.out.println(" at line " + ex.getLineNumber() 
-		      + ", column " + ex.getColumnNumber());
-		  } 
-		  catch (IOException ex) { 
-		    System.out.println("Due to an IOException, Xerces could not check " + DATEI_CONFIG);
-		  }
+	public Element getRootElement(File xmlFile){
+		
+		try {
+			Builder parser = new Builder();
+			Document doc = parser.build(xmlFile);
+			return doc.getRootElement();
+		} catch (ParsingException ex) {
+			ex.printStackTrace();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		
+		return null;
 	}
 }
