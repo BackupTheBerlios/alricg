1228b96197fc0770effe9e16b7057053d4f89366
vstrelow <vstrelow>
2005-03-04 10:59:49 +0000

    Lesen / schreiben von XML Daten möglich

--- alricg/src/org/d3s/alricg/CharKomponenten/Kultur.java
+++ alricg/src/org/d3s/alricg/CharKomponenten/Kultur.java
@@ -8,8 +8,11 @@
 package org.d3s.alricg.CharKomponenten;
 
 import nu.xom.Element;
+import nu.xom.Elements;
 
 import org.d3s.alricg.CharKomponenten.Links.Auswahl;
+import org.d3s.alricg.CharKomponenten.Links.AuswahlAusruestung;
+import org.d3s.alricg.CharKomponenten.Links.IdLinkList;
 import org.d3s.alricg.Controller.ProgAdmin;
 import org.d3s.alricg.Controller.CharKompAdmin.CharKomponenten;
 
@@ -20,13 +23,13 @@ import org.d3s.alricg.Controller.CharKompAdmin.CharKomponenten;
  * @author V.Strelow
  */
 public class Kultur extends Herkunft {
-    private Profession[] professionMoeglich;
-    private Profession[] professionUeblich;
+    private IdLinkList professionMoeglich;
+    private IdLinkList professionUeblich;
     private Auswahl muttersprache;
     private Auswahl zweitsprache;
     private Auswahl sprachen;
     private Auswahl schriften;
-    private Auswahl ausruestung;
+    private AuswahlAusruestung ausruestung;
     private RegionVolk regionVolk;
 
     private Kultur varianteVon; 
@@ -67,14 +70,14 @@ public class Kultur extends Herkunft {
     /**
      * @return Liefert das Attribut professionMoeglich.
      */
-    public Profession[] getProfessionMoeglich() {
+    public IdLinkList getProfessionMoeglich() {
         return professionMoeglich;
     }
 
     /**
      * @return Liefert das Attribut professionUeblich.
      */
-    public Profession[] getProfessionUeblich() {
+    public IdLinkList getProfessionUeblich() {
         return professionUeblich;
     }
 
@@ -104,15 +107,66 @@ public class Kultur extends Herkunft {
      */
     public void loadXmlElement(Element xmlElement) {
     	super.loadXmlElement(xmlElement);
+    	Elements tmpElements;
     	
+    	// Auslesen der Region
+    	if ( xmlElement.getFirstChildElement("region") != null ) {
+    		regionVolk = (RegionVolk) ProgAdmin.charKompAdmin.getCharElement(
+    				xmlElement.getFirstChildElement("region").getValue(),
+    				CharKomponenten.region
+    			);
+    	}
+    	
+    	// Auslesen der üblichen Professionen
+    	if ( xmlElement.getFirstChildElement("professionUeblich") != null ) {
+    		professionUeblich = new IdLinkList(this);
+    		professionUeblich.loadXmlElement(xmlElement
+    				.getFirstChildElement("professionUeblich"));
+    	}
+    	
+    	// Auslesen der möglichen Professionen
+    	if ( xmlElement.getFirstChildElement("professionMoeglich") != null ) {
+    		professionMoeglich = new IdLinkList(this);
+    		professionMoeglich.loadXmlElement(xmlElement
+    				.getFirstChildElement("professionMoeglich"));
+    	}
+    	
+    	// Auslesen der Muttersprache
+    	muttersprache = new Auswahl(this);
+    	muttersprache.loadXmlElement(xmlElement.getFirstChildElement("muttersprache"));
+    	
+    	// Auslesen der Zweitsprache
+		if ( xmlElement.getFirstChildElement("zweitsprache") != null ) {
+			zweitsprache = new Auswahl(this);
+			zweitsprache.loadXmlElement(xmlElement.getFirstChildElement("zweitsprache"));
+		}
+		
+		// Auslesen sonstiger Sprachen
+		if ( xmlElement.getFirstChildElement("sprachen") != null ) {
+			sprachen = new Auswahl(this);
+			sprachen.loadXmlElement(xmlElement.getFirstChildElement("sprachen"));
+		}
+		
+		// Auslesen er Schriften
+		if ( xmlElement.getFirstChildElement("schriften") != null ) {
+			schriften = new Auswahl(this);
+			schriften.loadXmlElement(xmlElement.getFirstChildElement("schriften"));
+		}
+		
+		// Auslesen der Ausrüstung
+		if ( xmlElement.getFirstChildElement("ausruestung") != null ) {
+			ausruestung = new AuswahlAusruestung(this);
+			ausruestung.loadXmlElement(xmlElement.getFirstChildElement("ausruestung"));
+		}
+		
+		
+		// Auslesen der Variante (gehört nach Schema eigentlich zur Herkunft)
 		if ( xmlElement.getFirstChildElement("varianteVon") !=  null ) {
 			varianteVon = (Kultur) ProgAdmin.charKompAdmin.getCharElement(
 	    			xmlElement.getFirstChildElement("varianteVon").getValue(),
 	    			CharKomponenten.kultur
 	    		);
 		}
-		
-    	// TODO implement
     }
     
     /* (non-Javadoc) Methode überschrieben
@@ -120,22 +174,66 @@ public class Kultur extends Herkunft {
      */
     public Element writeXmlElement(){
     	Element xmlElement = super.writeXmlElement();
-    	
+    	Element tmpElement;
     	int idx; 
-    	Element element;
     	
     	xmlElement.setLocalName("kultur");
     	
-    	// "varianteVon" schreiben
+    	// Schreiben der Region
+    	if ( regionVolk != null) {
+    		tmpElement = new Element("region");
+    		tmpElement.appendChild(regionVolk.getId());
+    		xmlElement.appendChild(tmpElement);
+    	}
+    	
+    	// Schreiben der üblichen Professionen
+    	if ( professionUeblich != null ) {
+    		xmlElement.appendChild(professionUeblich.writeXmlElement("professionUeblich"));
+    	}
+    	
+    	// Schreiben der möglichen Professionen
+    	if ( professionMoeglich != null ) {
+    		xmlElement.appendChild(professionMoeglich.writeXmlElement("professionMoeglich"));
+    	}
+    	
+    	// Schreiben der Muttersprache
+    	if ( muttersprache != null ) {
+    		xmlElement.appendChild(muttersprache.writeXmlElement("muttersprache"));
+    	}
+    	
+    	// Schreiben der Zweitsprache
+    	if ( zweitsprache != null ) {
+    		xmlElement.appendChild(zweitsprache.writeXmlElement("zweitsprache"));
+    	}
+    	
+    	// Schreiben der weiteren Sprachen
+    	if ( sprachen != null ) {
+    		xmlElement.appendChild(sprachen.writeXmlElement("sprachen"));
+    	}
+    	
+    	// Schreiben der Schriften
+    	if ( schriften != null ) {
+    		xmlElement.appendChild(schriften.writeXmlElement("schriften"));
+    	}
+    	
+    	// Schreiben der Schriften
+    	if ( ausruestung != null ) {
+    		xmlElement.appendChild(ausruestung.writeXmlElement("ausruestung"));
+    	}
+    	
+//    	 TODO implement
+    	
+    	// "varianteVon" schreiben (gehört nach Schema eigentlich zur Herkunft)
     	if ( this.varianteVon != null ) {
 	    	// hierfür muß die richtige Position bestimmt werden: 
 	    	idx = xmlElement.indexOf( xmlElement.getFirstChildElement("gp") );
-	    	element = new Element("varianteVon");
-	    	element.appendChild(this.varianteVon.getId());
+	    	tmpElement = new Element("varianteVon");
+	    	tmpElement.appendChild(this.varianteVon.getId());
 	    	
 	    	// einfügen nach dem "gp" Element!
-	    	xmlElement.insertChild(element, idx+1);
+	    	xmlElement.insertChild(tmpElement, idx+1);
     	}
+    	
 		return xmlElement;
     }
 }
--- alricg/src/org/d3s/alricg/CharKomponenten/Profession.java
+++ alricg/src/org/d3s/alricg/CharKomponenten/Profession.java
@@ -7,9 +7,15 @@
 
 package org.d3s.alricg.CharKomponenten;
 
+import nu.xom.Attribute;
 import nu.xom.Element;
+import nu.xom.Elements;
 
 import org.d3s.alricg.CharKomponenten.Links.Auswahl;
+import org.d3s.alricg.CharKomponenten.Links.AuswahlAusruestung;
+import org.d3s.alricg.CharKomponenten.Links.IdLinkList;
+import org.d3s.alricg.CharKomponenten.Werte.Gilde;
+import org.d3s.alricg.CharKomponenten.Werte.MagieMerkmal;
 import org.d3s.alricg.Controller.ProgAdmin;
 import org.d3s.alricg.Controller.CharKompAdmin.CharKomponenten;
 /**
@@ -19,19 +25,52 @@ import org.d3s.alricg.Controller.CharKompAdmin.CharKomponenten;
  * @author V.Strelow
  */
 public class Profession extends Herkunft {
+   
+    public enum Art { 
+    	handwerklich("handwerklich"), 
+    	kriegerisch("kriegerisch"), 
+    	gesellschaftlich("gesellschaftlich"),
+    	wildnis("wildnis"), 
+    	magisch("magisch"), 
+    	geweiht("geweiht"), 
+    	schamanisch("schamanisch");
+    	
+		private String xmlValue; // XML-Tag des Elements
+		
+		private Art(String xmlValue) {
+			this.xmlValue = xmlValue;
+		}
+		
+		public String getXmlValue() {
+			return xmlValue;
+		}
+    }
+    public enum Aufwand { 
+    	erstprof("erstprofession"), 
+    	zeitaufw("zeitaufwendig"),
+    	normal("-");
     
-    public enum Art { HANDWERKLICH, KRIEGERISCH, GESELLSCHAFTLICH, WILDNESS, MAGISCH, GEWEIHT, SCHAMANISCH }
-    public enum Aufwand { ERSTPROF, ZEITAUFW }
-    
+		private String xmlValue; // XML-Tag des Elements
+		
+		private Aufwand(String xmlValue) {
+			this.xmlValue = xmlValue;
+		}
+		
+		public String getXmlValue() {
+			return xmlValue;
+		} 
+    }
+	
 	private Aufwand aufwand;
 	private Art art;
-	private Vorteil[] verbotenVort;
-	private Nachteil[] verbotenNacht;
-	private Sonderfertigkeit[] verbotenSF;
+	private MagierAkademie magierAkademie;
+	private IdLinkList verbotenVort;
+	private IdLinkList verbotenNacht;
+	private IdLinkList verbotenSF;
 	private Auswahl sprachen;
 	private Auswahl schriften;
-	private Auswahl ausruestung;
-	private Auswahl besondererBesitz;
+	private AuswahlAusruestung ausruestung;
+	private AuswahlAusruestung besondererBesitz;
 	
     private Profession varianteVon;
     
@@ -51,11 +90,11 @@ public class Profession extends Herkunft {
 
 	public Art getArt(){ return art; }
 
-	public Vorteil[] getVerbotenVort(){ return verbotenVort; }
+	public IdLinkList getVerbotenVort(){ return verbotenVort; }
 
-	public Nachteil[] getVerbotenNacht(){ return verbotenNacht; }
+	public IdLinkList getVerbotenNacht(){ return verbotenNacht; }
 
-	public Sonderfertigkeit[] getVerbotenSF(){ return verbotenSF; }
+	public IdLinkList getVerbotenSF(){ return verbotenSF; }
 
 	public Auswahl getSprachen(){ return sprachen; }
 
@@ -65,12 +104,103 @@ public class Profession extends Herkunft {
 
 	public Auswahl getBesondererBesitz(){ return besondererBesitz; }
 
+	
+	/**
+	 * Liefert zu einem XML-Tag die entsprechende Enum der Prof-Art zurück.
+	 * @param xmlValue Der XML-Tag art aus dem Element Profession
+	 * @return Die Enum Art die zu den xmlTag gehört
+	 */
+	private Art getArtByXmlTag(String xmlValue) {
+		Art[] artArray = Art.values();
+		
+		// Suchen des richtigen Elements
+		for (int i = 0; i < artArray.length; i++) {
+			if (xmlValue.equals(artArray[i].getXmlValue())) {
+				return artArray[i]; // Gefunden
+			}
+		}
+		
+		ProgAdmin.logger.severe("XmlValue konnte nicht gefunden werden!");
+		
+		return null;
+		
+	}
+	
     /* (non-Javadoc) Methode überschrieben
      * @see org.d3s.alricg.CharKomponenten.CharElement#loadXmlElement(nu.xom.Element)
      */
     public void loadXmlElement(Element xmlElement) {
     	super.loadXmlElement(xmlElement);
     	
+    	// Auslesen des Attribus "aufwand"
+    	if ( xmlElement.getAttribute("aufwand") != null ) {
+    		
+    		// Sicherstellen das der Wertebereich gültig ist:
+    		assert xmlElement.getAttributeValue("aufwand")
+    									.equals(Aufwand.erstprof.getXmlValue())
+					|| xmlElement.getAttributeValue("aufwand")
+										.equals(Aufwand.zeitaufw.getXmlValue());
+    		
+    		if ( xmlElement.getAttributeValue("aufwand")
+    				.equals(Aufwand.erstprof.getXmlValue()) ) {
+    			aufwand = Aufwand.erstprof;
+    		} else {
+    			aufwand = Aufwand.zeitaufw;
+    		}
+    	}
+    	
+    	// Auslesen der Art dieser Profession
+    	this.art = getArtByXmlTag(xmlElement.getFirstChildElement("art").getValue());
+    	
+    	// Auslesen der MagierAkademie, sofern vorhanden
+    	if ( xmlElement.getFirstChildElement("magierAkademie") != null ) {
+    		magierAkademie = new MagierAkademie(this);
+    		magierAkademie.loadXmlElement(xmlElement.getFirstChildElement("magierAkademie"));
+    	}
+    	
+    	// Auslesen der verbotenen Vorteile
+    	if ( xmlElement.getFirstChildElement("verboteVT") != null ) {
+    		verbotenVort = new IdLinkList(this);
+    		verbotenVort.loadXmlElement(xmlElement.getFirstChildElement("verboteVT"));
+    	}
+    	
+    	// Auslesen der verbotenen Nachteile
+    	if ( xmlElement.getFirstChildElement("verboteNT") != null ) {
+    		verbotenNacht = new IdLinkList(this);
+    		verbotenNacht.loadXmlElement(xmlElement.getFirstChildElement("verboteNT"));
+    	}
+    	
+    	// Auslesen der verbotenen Sonderfertigkeiten
+    	if ( xmlElement.getFirstChildElement("verboteSF") != null ) {
+    		verbotenSF = new IdLinkList(this);
+    		verbotenSF.loadXmlElement(xmlElement.getFirstChildElement("verboteSF"));
+    	}
+    	
+    	// Auslesen der Sprachen Auswahl
+    	if ( xmlElement.getFirstChildElement("sprachen") != null ) {
+    		sprachen = new Auswahl(this);
+    		sprachen.loadXmlElement(xmlElement.getFirstChildElement("sprachen"));
+    	}
+    	
+    	// Auslesen der Schriften Auswahl
+    	if ( xmlElement.getFirstChildElement("schriften") != null ) {
+    		schriften = new Auswahl(this);
+    		schriften.loadXmlElement(xmlElement.getFirstChildElement("schriften"));
+    	}
+    	
+    	// Auslesen der Ausrüstung Auswahl
+    	if ( xmlElement.getFirstChildElement("ausruestung") != null ) {
+    		ausruestung = new AuswahlAusruestung(this);
+    		ausruestung.loadXmlElement(xmlElement.getFirstChildElement("ausruestung"));
+    	}
+    	
+    	// Auslesen der besondere Besitz Auswahl
+    	if ( xmlElement.getFirstChildElement("besondererBesitz") != null ) {
+    		besondererBesitz = new AuswahlAusruestung(this);
+    		besondererBesitz.loadXmlElement(xmlElement.getFirstChildElement("besondererBesitz"));
+    	}
+    	
+    	// Lesen der Varaiante (eigentlich von Herkunft)
 		if ( xmlElement.getFirstChildElement("varianteVon") !=  null ) {
 			varianteVon = (Profession) ProgAdmin.charKompAdmin.getCharElement(
 	    			xmlElement.getFirstChildElement("varianteVon").getValue(),
@@ -86,34 +216,200 @@ public class Profession extends Herkunft {
      */
     public Element writeXmlElement(){
     	Element xmlElement = super.writeXmlElement();
-    	
+    	Element tmpElement;
     	int idx; 
-    	Element element;
     	
     	xmlElement.setLocalName("profession");
     	
-    	// "varianteVon" schreiben
+    	// Schreiben des Attributs "Aufwand", wenn nötig
+    	if ( aufwand != Aufwand.normal ) {
+    		xmlElement.addAttribute(new Attribute("aufwand", aufwand.getXmlValue()));
+    	}
+    	
+    	// Schreiben der Art der Profession
+    	tmpElement = new Element("art");
+    	tmpElement.appendChild(art.getXmlValue());
+    	xmlElement.appendChild(tmpElement);
+    	
+    	// Schreiben der MagierAkademie
+    	if ( magierAkademie != null ) {
+    		tmpElement.appendChild(magierAkademie.writeXmlElement("magierAkademie"));
+    	}
+    	
+    	// Schreiben der verbotenen Vorteile
+    	if ( verbotenVort != null ) {
+    		tmpElement.appendChild(verbotenVort.writeXmlElement("verboteVT"));
+    	}
+    	
+    	// Schreiben der verbotenen Nachteile
+    	if ( verbotenNacht != null ) {
+    		tmpElement.appendChild(verbotenNacht.writeXmlElement("verboteNT"));
+    	}
+    	
+    	// Schreiben der verbotenen Sonderfertigkeiten
+    	if ( verbotenSF != null ) {
+    		tmpElement.appendChild(verbotenSF.writeXmlElement("verboteSF"));
+    	}
+    	
+    	// Schreiben der Sprachen
+    	if ( sprachen != null ) {
+    		tmpElement.appendChild(sprachen.writeXmlElement("sprachen"));
+    	}
+    	
+    	// Schreiben der Schriften
+    	if ( schriften != null ) {
+    		tmpElement.appendChild(schriften.writeXmlElement("schriften"));
+    	}
+    	
+    	// Schreiben der Ausrüstung
+    	if ( ausruestung != null ) {
+    		tmpElement.appendChild(ausruestung.writeXmlElement("ausruestung"));
+    	}
+    	
+    	// Schreiben des besonderen Besitzens
+    	if ( besondererBesitz != null ) {
+    		tmpElement.appendChild(besondererBesitz.writeXmlElement("besondererBesitz"));
+    	}
+    	
+
+    	// "varianteVon" schreiben (eigentlich von Herkunft)
     	if ( this.varianteVon != null ) {
 	    	// hierfür muß die richtige Position bestimmt werden: 
 	    	idx = xmlElement.indexOf( xmlElement.getFirstChildElement("gp") );
-	    	element = new Element("varianteVon");
-	    	element.appendChild(this.varianteVon.getId());
+	    	tmpElement = new Element("varianteVon");
+	    	tmpElement.appendChild(this.varianteVon.getId());
 	    	
 	    	// einfügen nach dem "gp" Element!
-	    	xmlElement.insertChild(element, idx+1);
+	    	xmlElement.insertChild(tmpElement, idx+1);
     	}
     	
     	// TODO Implementieren
     	
-    	return null;
+    	return xmlElement;
     }
-	
-	private static class MagierAkademie {        	
-		private int gilde;
-    	private int besondereMerkmale[];
-    	private boolean zweitStudiumMoeglich;
-        private boolean drittStudiumMoeglich;
+    
+    /**
+     * 
+     * <u>Beschreibung:</u><br> 
+     * Repräsentiert die üblichen angaben die für eine Magier-Akedemie benötigt werden,
+     * wenn diese als Profession gewählt werden kann.
+     * @author V. Strelow
+     */
+	public class MagierAkademie {        	
+		private Gilde gilde;
+    	private MagieMerkmal merkmale[];
+    	private boolean zweitStudiumMoeglich = true;
+        private boolean drittStudiumMoeglich = false;
+        private String anmerkung;
+        private Profession herkunft;
+        
+        /**
+         * Konstruktor
+         * @param herkunft Die Profession, von der diese MagierAkademie stammt.
+         */
+        MagierAkademie(Profession herkunft) {
+        	this.herkunft = herkunft;
+        }
+        
+        /**
+         * Dient zum initialisieren des Objekts. Ein XML-Elements wird gegeben, daraus
+         * werden alle relevanten Informationen ausgelesen.
+         * @param xmlElement Das Xml-Element mit allen nötigen angaben
+         */
+        public void loadXmlElement(Element xmlElement) {
+        	Elements tmpElements;
+        	
+        	// Auslesen der Gildenzugehörigkeit
+        	gilde = Werte.getGildeByXmlValue(
+        				xmlElement.getFirstChildElement("gilde").getValue()
+        			);
+        	
+        	// Auslesen der Merkmale der Akademie
+        	tmpElements = xmlElement.getChildElements("merkmale");
+        	merkmale = new MagieMerkmal[tmpElements.size()];
+        	
+        	for (int i = 0; i < merkmale.length; i++) {
+        		merkmale[i] = Werte.getMagieMerkmalByXmlValue(
+        							tmpElements.get(i).getValue()
+        						);
+        	}
+        	
+        	// Auslesen ob ein Zweitstudium möglich ist
+        	if ( xmlElement.getFirstChildElement("zweitStudiumMoeglich") != null ) {
+        		
+        		// Überprüfung ob es dem Wertebereich entspricht
+        		assert xmlElement.getFirstChildElement("zweitStudiumMoeglich").equals("false")
+        			|| xmlElement.getFirstChildElement("zweitStudiumMoeglich").equals("true");
+        		
+        		if (xmlElement.getFirstChildElement("zweitStudiumMoeglich").equals("false")) {
+        			zweitStudiumMoeglich = false;
+        		}
+        	}
+        	
+        	// Auslesen ob ein Drittstudium möglich ist
+        	if ( xmlElement.getFirstChildElement("drittStudiumMoeglich") != null ) {
+        		
+        		// Überprüfung ob es dem Wertebereich entspricht
+        		assert xmlElement.getFirstChildElement("drittStudiumMoeglich").equals("false")
+        			|| xmlElement.getFirstChildElement("drittStudiumMoeglich").equals("true");
+        		
+        		if (xmlElement.getFirstChildElement("drittStudiumMoeglich").equals("true")) {
+        			drittStudiumMoeglich = true;
+        		}
+        	}
+        	
+        	// Anmerkungen zu der Akademie auslesen
+        	if ( xmlElement.getFirstChildElement("anmerkung") != null ) {
+        		anmerkung = xmlElement.getFirstChildElement("anmerkung").getValue();
+        	}
+        	
+        }
+        
+        /**
+         * Dient zur Speicherung (also für den Editor) des Objekts. Alle Angaben werden 
+         * in ein XML Objekt "gemapt".
+         * @param tagName Der name des Tags
+         * @return Ein Xml-Element mit allen nötigen Angaben.
+         */
+        public Element writeXmlElement(String xmlTag){
+        	Element xmlElement = new Element(xmlTag);
+        	Element tmpElement;
+        	
+        	// Schreiben der Gilde
+        	tmpElement = new Element("gilde");
+        	tmpElement.appendChild(gilde.getXmlValue());
+        	xmlElement.appendChild(tmpElement);
+        	
+        	// Schreiben der magischen Merkmale
+        	for (int i = 0; i < merkmale.length; i++) {
+            	tmpElement = new Element("merkmale");
+            	tmpElement.appendChild(merkmale[i].getXmlValue());
+            	xmlElement.appendChild(tmpElement);
+        	}
+        	
+        	// Schreiben ob Zweitstudium möglich
+        	if ( !zweitStudiumMoeglich )  {
+            	tmpElement = new Element("zweitStudiumMoeglich");
+            	tmpElement.appendChild("false");
+            	xmlElement.appendChild(tmpElement);
+        	}
+        	
+        	// Schreiben ob Drittstudium möglich
+        	if ( drittStudiumMoeglich )  {
+            	tmpElement = new Element("drittStudiumMoeglich");
+            	tmpElement.appendChild("true");
+            	xmlElement.appendChild(tmpElement);
+        	}
+        	
+        	// Schreiben der Anmerkung zur Akademie
+        	if ( anmerkung != null && anmerkung.trim().length() > 0 ) {
+            	tmpElement = new Element("anmerkung");
+            	tmpElement.appendChild(anmerkung.trim());
+            	xmlElement.appendChild(tmpElement);
+        	}
+        	
+        	return xmlElement;
+        }
+        
 	}
-
-
 }
