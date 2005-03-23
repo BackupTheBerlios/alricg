79c6862a3787dab2c95ddad812e6c901cde007ab
vstrelow <vstrelow>
2005-03-23 19:51:16 +0000

    Lesen / schreiben von XML

--- alricg/src/org/d3s/alricg/CharKomponenten/Gabe.java
+++ alricg/src/org/d3s/alricg/CharKomponenten/Gabe.java
@@ -1,12 +1,15 @@
 /*
  * Created 20. Januar 2005 / 16:24:31
  * This file is part of the project ALRICG. The file is copyright
- * protected an under the GNU General Public License.
+ * protected and under the GNU General Public License.
  * For more information see "http://alricg.die3sphaere.de/".
  */
 
 package org.d3s.alricg.CharKomponenten;
 
+import org.d3s.alricg.Controller.ProgAdmin;
+
+import nu.xom.Attribute;
 import nu.xom.Element;
 
 /**
@@ -16,7 +19,9 @@ import nu.xom.Element;
  * @author V.Strelow
  */
 public class Gabe extends Faehigkeit {
-    
+	private int minStufe = 1;
+	private int maxStufe = 1;
+	
 	/**
 	 * Konstruktur; id beginnt mit "GAB-" für Gabe
 	 * @param id Systemweit eindeutige id
@@ -30,7 +35,27 @@ public class Gabe extends Faehigkeit {
      */
     public void loadXmlElement(Element xmlElement) {
     	super.loadXmlElement(xmlElement);
-    	// Noop!
+	    
+    	// Auslesen der Stufengrenzen
+    	try {
+	    	if ( xmlElement.getFirstChildElement("stufenGrenzen") != null ) {
+	    		if ( xmlElement.getFirstChildElement("stufenGrenzen")
+	    				.getAttribute("minStufe") != null ) 
+	    		{
+	    			minStufe = Integer.parseInt(xmlElement.getFirstChildElement("stufenGrenzen")
+		    				.getAttributeValue("minStufe"));
+	    		}
+	    		if ( xmlElement.getFirstChildElement("stufenGrenzen")
+	    				.getAttribute("maxStufe") != null ) 
+	    		{
+	    			maxStufe = Integer.parseInt(xmlElement.getFirstChildElement("stufenGrenzen")
+		    				.getAttributeValue("maxStufe"));
+	    		}
+	    	}
+		} catch (NumberFormatException exc) {
+			// TODO Bessere Fehlermeldung einbauen
+			ProgAdmin.logger.severe("Fehler bei Umrechnung von Zahlen: " + exc);
+		}
     }
     
     /* (non-Javadoc) Methode überschrieben
@@ -38,7 +63,23 @@ public class Gabe extends Faehigkeit {
      */
     public Element writeXmlElement(){
     	Element xmlElement = super.writeXmlElement();
-//    	 Noop!
+    	Element tmpElement;
+    	
+    	xmlElement.setLocalName("gabe");
+    	
+    	//schreiben der Stufengrenzen
+    	if ( minStufe != 1 || maxStufe != 1 ) {
+    		tmpElement = new Element("stufenGrenzen");
+    		
+    		if ( minStufe != 1 ) {
+    			tmpElement.addAttribute(new Attribute("minStufe", Integer.toString(minStufe)));
+    		}
+    		if ( maxStufe != 1 ) {
+    			tmpElement.addAttribute(new Attribute("maxStufe", Integer.toString(maxStufe)));
+    		}
+    		xmlElement.appendChild(tmpElement);
+    	}
+    	
     	return xmlElement;
     }
 }
