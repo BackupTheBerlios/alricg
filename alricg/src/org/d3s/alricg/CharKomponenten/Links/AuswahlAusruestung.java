ccdf559044b0829733764662b95b0a27c3981a94
vstrelow <vstrelow>
2005-03-23 19:42:44 +0000

    Schreiben in XML berichtigt bei direkt definierten Gegenständen

--- alricg/src/org/d3s/alricg/CharKomponenten/Links/AuswahlAusruestung.java
+++ alricg/src/org/d3s/alricg/CharKomponenten/Links/AuswahlAusruestung.java
@@ -14,23 +14,25 @@ import nu.xom.Elements;
 
 import org.d3s.alricg.CharKomponenten.Herkunft;
 import org.d3s.alricg.CharKomponenten.CharZusatz.SimpelGegenstand;
+import org.d3s.alricg.Controller.ProgAdmin;
 
 /**
  * <u>Beschreibung:</u><br> 
- *
+ * Repräsentiert eine Auswahl von Ausrüstungsgegenständen für Kultur / Professionen
  * @author V. Strelow
  */
-public class AuswahlAusruestung extends Auswahl {
-	//protected VariableAuswahl[] varianteAuswahl;
-    //private Herkunft herkunft; // Das CharElement, von dem die Auswahl kommt
-	//private IdLink[] festeAuswahl; // Die unveränderlichen Werte
+public class AuswahlAusruestung {
+	
+	private Herkunft herkunft;
+	private HilfsAuswahl festeAuswahl; // Die unveränderlichen Werte
+	private HilfsAuswahl[] variableAuswahl;
 	
 	/**
 	 * Konstruktor
 	 * @param herkunft Die "quelle" dieser Auswahl
 	 */
 	public AuswahlAusruestung(Herkunft herkunft) {
-		super(herkunft);
+		this.herkunft = herkunft;
 	}
 	
     /**
@@ -39,55 +41,21 @@ public class AuswahlAusruestung extends Auswahl {
      * @param xmlElement Das Xml-Element mit allen nötigen angaben
      */
     public void loadXmlElement(Element xmlElement) {
-    	Elements tmpElements1, tmpElements2, tmpElementsX;
-    	IdLink[] optionen;
+    	Elements tmpElements;
     	int anzahl;
     	
     	// Auslesen der festen Gegenstände
     	if ( xmlElement.getFirstChildElement("fest") != null) {
-    		
-    		tmpElements1 = xmlElement.getFirstChildElement("fest")
-    								.getChildElements("ausruestungLink");
-    		tmpElements2 = xmlElement.getFirstChildElement("fest")
-									.getChildElements("ausruestungNeu");
-    		
-    		festeAuswahl = new IdLink[tmpElements1.size() + tmpElements2.size()];
-    
-//    		Auslesen der XML-Tags
-    		loadHelp(festeAuswahl, tmpElements1, tmpElements2);
+    		festeAuswahl = new HilfsAuswahl();
+    		festeAuswahl.loadXmlElement(xmlElement.getFirstChildElement("fest"), herkunft);
     	}
     	
-// 		Auslesen der Variablen Auswahl der Gegenstande
-    	if ( xmlElement.getFirstChildElement("auswahl") != null ) {
-    		tmpElementsX = xmlElement.getChildElements("auswahl");
-    		
-    		varianteAuswahl = new VariableAuswahl[tmpElementsX.size()];
-    		
-    		for ( int idx = 0; idx < tmpElementsX.size(); idx++) {
-	    		tmpElements1 = tmpElementsX.get(idx).getChildElements("ausruestungLink");
-				tmpElements2 = tmpElementsX.get(idx).getChildElements("ausruestungNeu");
-				
-				optionen = new IdLink[tmpElements1.size() + tmpElements2.size()];
-				
-				// Auslesen der XML-Tags
-				loadHelp(optionen, tmpElements1, tmpElements2);
-	    		
-	    		if (tmpElementsX.get(idx).getAttribute("anzahl") != null) {
-	    			anzahl = Integer.parseInt(tmpElementsX.get(idx)
-	    										.getAttributeValue("anzahl"));
-	    		} else {
-	    			anzahl = 1;
-	    		}
-	    		
-	    		// Schreiben der varianten Auswahl
-	    		varianteAuswahl[idx] = new VariableAuswahl(
-	    					Modus.ANZAHL, 
-	    					new int[0],
-	    					anzahl,
-	    					optionen,
-	    					null
-	    			);
-    		}
+    	// Auslesen der Auswahl
+    	tmpElements = xmlElement.getChildElements("auswahl");
+    	variableAuswahl = new HilfsAuswahl[tmpElements.size()];
+    	for ( int i = 0; i < variableAuswahl.length; i++) {
+    		variableAuswahl[i] = new HilfsAuswahl();
+    		variableAuswahl[i].loadXmlElement(tmpElements.get(i), herkunft);
     	}
     }
     	
@@ -99,107 +67,91 @@ public class AuswahlAusruestung extends Auswahl {
      */
     public Element writeXmlElement(String tagName){
     	Element xmlElement = new Element(tagName);
-    	Element tmpElement;
     	
-    	// Schreiben der "festen" Elemente, die nicht wählbar sind
-    	if ( festeAuswahl != null ) {
-    		
-    		tmpElement = new Element("fest");
-    		xmlElement.appendChild(tmpElement);
-    		
-    		// Schreiben der Daten:
-    		writerHelp(festeAuswahl, tmpElement);
- 
+    	// Schreiben der "festen" Elemente
+    	if (festeAuswahl != null) {
+    		xmlElement.appendChild(festeAuswahl.writeXmlElement("fest"));
     	}
     	
-    	// Schreiben der Elemente, von denen eine Auswahl getroffen werden kann
-    	if ( varianteAuswahl != null) {
-
-    		// Durchlaufen der verschiedenen Auswahlen
-    		for ( int i = 0; i < varianteAuswahl.length; i++) {
-        		tmpElement = new Element("auswahl");
-        		xmlElement.appendChild(tmpElement);
-    			
-    			// Schreiben des Attributs "anzahl"
-    			tmpElement.addAttribute( new Attribute("anzahl", 
-    					Integer.toString(varianteAuswahl[i].getAnzahl())) );
-    			
-    			// Schreiben der eigentlichen daten
-        		writerHelp(varianteAuswahl[i].getOptionen(), tmpElement);
-    		}
+    	// Schreiben der "wählbaren" Elemente
+    	for (int i = 0; i < variableAuswahl.length; i++) {
+    		xmlElement.appendChild(variableAuswahl[i].writeXmlElement("auswahl"));
     	}
     	
     	return xmlElement;
     }
     
     /**
-     * Hilfsfunktion für das lesen von XML
-     * @param linkAR In dieses Array werden die ausgelesenen Daten geschrieben
-     * @param tmpElements1 XML-Elemente die zu anderen Elementen linken
-     * @param tmpElements2 XML-Elemente die im Tag selbst Definiert werden
+     * <u>Beschreibung:</u><br> 
+     *  Hilfklasse für die Datenhaltung
+     * @author V. Strelow
      */
-    private void loadHelp(IdLink[] linkAR, Elements tmpElements1, Elements tmpElements2) {
-    	SimpelGegenstand simpelGegen;
-    	int tmpAnzahl;
+    private class HilfsAuswahl  {
+    	private int anzahl = 1;
+    	private IdLink[] links;
+    	private SimpelGegenstand[] simpGegenstand;
     	
-		// Auslesen der Gegenstände, die verlinkt werden	
-		for (int i = 0; i < tmpElements1.size(); i++) {
-			linkAR[i] = new IdLink(this.getHerkunft(), this);
-			linkAR[i].loadXmlElement( tmpElements1.get(i) );
-		}
-		
-		// Auslesen der Gegenstände, die erst hier Definiert werden
-		for (int i = 0; i < tmpElements2.size(); i++) {
-			simpelGegen = new SimpelGegenstand();
-			simpelGegen.loadXmlElement(tmpElements2.get(i));
-			
-			linkAR[i+tmpElements1.size()] = new IdLink(this.getHerkunft(), this);
-			linkAR[i+tmpElements1.size()].setZielId( simpelGegen );
-			
-			// Anzahl setzen
-			if ( tmpElements2.get(i).getAttribute("anzahl") != null) {
-				tmpAnzahl = Integer.parseInt(tmpElements2.get(i).getAttributeValue("anzahl"));
-			} else {
-				tmpAnzahl = 1; // Defaultz wert
-			} 
-			linkAR[i+tmpElements1.size()].setWert(tmpAnzahl);
-		}
+    	/**
+    	 * Liest aus einem XML-Element alle Daten ein
+    	 * @param xmlElement Das zu lesende Element
+    	 * @param herkunft Die Herkunft diese Auswahl
+    	 */
+		public void loadXmlElement(Element xmlElement, Herkunft herkunft) {
+	    	Elements tmpElements;
 
-    }
-    
-    /**
-     * Hilffunktion für das Schreiben von XML
-     * @param linkAR Array von LinkIds, das gelesen wird um zu schreiben
-     * @param xmlElement Aus diesem xmlElement wird ausgelesen
-     */
-    private void writerHelp(IdLink[] linkAR, Element xmlElement) {
-    	Element tmpElement;
-    	
-		// Schreiben der Gegenstände MIT einer verlinkung zur Datenbasis
-		for (int i = 0; i < linkAR.length; i++) {
-			if ( !linkAR[i].getZielId().equals(SimpelGegenstand.TEMP_ID) ) {
-				xmlElement.appendChild( 
-						linkAR[i].writeXmlElement("ausruestungLink")
-					);
-			}
+	    	// Die Anzahl auslesen, falls angegeben?
+	    	if ( xmlElement.getAttribute("anzahl") != null) {
+	    		try {
+	    			anzahl = Integer.parseInt(xmlElement.getAttributeValue("anzahl"));
+	    		} catch (NumberFormatException exc) {
+	    			ProgAdmin.logger.severe("Konnte Sting nicht in int umwandeln: " + exc);
+	    		}
+	    	}
+	    	
+	    	// Auslesen der Gegenstände die verlinkt werden!
+	    	tmpElements = xmlElement.getChildElements("ausruestungLink");
+    		links = new IdLink[tmpElements.size()];
+    		for (int i = 0; i < tmpElements.size(); i++) {
+    			
+    			// ACHTUNG Die "null" an der Stelle könnte zu Problemen führen!!
+    			links[i] = new IdLink(herkunft, null);
+    			links[i].loadXmlElement( tmpElements.get(i) );
+    		}
+    		
+    		// Auslesen der simplen Gegenstände, die direkt angegeben werden
+    		tmpElements = xmlElement.getChildElements("ausruestungNeu");
+    		simpGegenstand = new SimpelGegenstand[tmpElements.size()];
+    		for (int i = 0; i < tmpElements.size(); i++) {
+    			simpGegenstand[i] = new SimpelGegenstand();
+    			simpGegenstand[i].loadXmlElement( tmpElements.get(i) );
+    		}
 		}
 		
-		// Schreiben der Gegenstände die direkt Definiert werden
-		for (int i = 0; i < linkAR.length; i++) {
-			if ( linkAR[i].getZielId().equals(SimpelGegenstand.TEMP_ID) ) {
-				tmpElement = ((SimpelGegenstand) linkAR[i].getZielId())
-											.writeXmlElement("ausruestungNeu");
-				
-				// Anzahl setzen, wenn nicht Default (= 1)
-				if ( linkAR[i].getWert() != 1 ) {
-					tmpElement.addAttribute(new Attribute("anzahl", 
-								Integer.toString(linkAR[i].getWert())		
-							));	
-				}
-				
-				// Schreibe Wert
-				xmlElement.appendChild(tmpElement);
-			}
-		}
+		/**
+		 * Schreib alle Daten in ein XML-Element mit dem gegebenem Tag-Namen
+		 * @param tagName Der Name des Tags
+		 * @return xml-Element mit allen Daten.
+		 */
+        public Element writeXmlElement(String tagName){
+        	Element xmlElement = new Element(tagName);
+        	Element tmpElement;
+        	
+        	// Schreiben der Anzahl
+        	if ( anzahl != 1 ) {
+        		xmlElement.addAttribute(new Attribute("anzahl", Integer.toString(anzahl)));
+        	}
+        	
+        	// Schreiben der Links
+        	for (int i = 0; i < links.length; i++) {
+        		xmlElement.appendChild(links[i].writeXmlElement("ausruestungLink"));
+        	}
+        	
+        	// Schreiben der simplen Gegenstände, die direkt angegeben werden
+        	for (int i = 0; i < simpGegenstand.length; i++) {
+        		xmlElement.appendChild(simpGegenstand[i].writeXmlElement("ausruestungNeu"));
+        	}
+        	
+        	return xmlElement;
+        }
     }
 }
