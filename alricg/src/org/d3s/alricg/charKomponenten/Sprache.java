df6bdff8057e0595202e4be006de22e6560b7d8f
msturzen <msturzen>
2005-05-15 22:22:52 +0000

    Riesen-Refactoring für store; step 1

--- alricg/src/org/d3s/alricg/charKomponenten/Sprache.java
+++ alricg/src/org/d3s/alricg/charKomponenten/Sprache.java
@@ -7,127 +7,80 @@
 
 package org.d3s.alricg.charKomponenten;
 
-import nu.xom.Attribute;
-import nu.xom.Element;
-
 import org.d3s.alricg.charKomponenten.links.IdLinkList;
 import org.d3s.alricg.controller.CharKomponente;
-import org.d3s.alricg.controller.ProgAdmin;
-import org.d3s.alricg.prozessor.FormelSammlung;
 import org.d3s.alricg.prozessor.FormelSammlung.KostenKlasse;
 
 /**
  * <b>Beschreibung:</b><br>
  * Repräsentiert eine Sprache.
+ * 
  * @author V.Strelow
  */
 public class Sprache extends SchriftSprache {
-	private int komplexNichtMutterSpr;
-	private KostenKlasse kostenNichtMutterSpr;
-	private IdLinkList zugehoerigeSchrift;	
+    private int komplexNichtMutterSpr;
 
-	/* (non-Javadoc) Methode überschrieben
-	 * @see org.d3s.alricg.charKomponenten.CharElement#getCharKomponente()
-	 */
-	public CharKomponente getCharKomponente() {
-		return CharKomponente.sprache;
-	}
-	
-	/**
-	 * Konstruktur; id beginnt mit "SPR-" für Sprache
-	 * @param id Systemweit eindeutige id
-	 */
-	public Sprache(String id) {
-		setId(id);
-	}
-	
-	/**
-	 * @return Liefert das Attribut zugehoerigeSchrift.
-	 */
-	public IdLinkList getZugehoerigeSchrift() {
-		return zugehoerigeSchrift;
-	}
-	
-	/**
-	 * @return Liefert das Attribut komplexNichtMutterSpr.
-	 */
-	public int getKomplexNichtMutterSpr() {
-		return komplexNichtMutterSpr;
-	}
-	/**
-	 * @return Liefert das Attribut kostenNichtMutterSpr.
-	 */
-	public KostenKlasse getKostenNichtMutterSpr() {
-		return kostenNichtMutterSpr;
-	}
-	
-	/**
-	 * @param komplexNichtMutterSpr Setzt das Attribut komplexNichtMutterSpr.
-	 */
-	public void setKomplexNichtMutterSpr(int komplexNichtMutterSpr) {
-		this.komplexNichtMutterSpr = komplexNichtMutterSpr;
-	}
-	/**
-	 * @param kostenNichtMutterSpr Setzt das Attribut kostenNichtMutterSpr.
-	 */
-	public void setKostenNichtMutterSpr(KostenKlasse kostenNichtMutterSpr) {
-		this.kostenNichtMutterSpr = kostenNichtMutterSpr;
-	}
-	/**
-	 * @param zugehoerigeSchrift Setzt das Attribut zugehoerigeSchrift.
-	 */
-	public void setZugehoerigeSchrift(IdLinkList zugehoerigeSchrift) {
-		this.zugehoerigeSchrift = zugehoerigeSchrift;
-	}
-    /* (non-Javadoc) Methode überschrieben
-     * @see org.d3s.alricg.charKomponenten.CharElement#loadXmlElement(nu.xom.Element)
+    private KostenKlasse kostenNichtMutterSpr;
+
+    private IdLinkList zugehoerigeSchrift;
+
+    /*
+     * (non-Javadoc) Methode überschrieben
+     * 
+     * @see org.d3s.alricg.charKomponenten.CharElement#getCharKomponente()
      */
-    public void loadXmlElement(Element xmlElement) {
-    	super.loadXmlElement(xmlElement);
-    	
-    	// Auslesen der Kostenklasse der Sprache
-    		kostenNichtMutterSpr = FormelSammlung.getKostenKlasseByXmlValue( xmlElement
-    			.getFirstChildElement("nichtMuttersprache").getAttributeValue("kostenKlasse") );
-    	
-    	// Auslesen der Kompläxität der Sprache
-    	try {
-    		komplexNichtMutterSpr = Integer.parseInt( xmlElement
-    			.getFirstChildElement("nichtMuttersprache").getAttributeValue("komplexitaet") );
-    	} catch (NumberFormatException exc) {
-    		ProgAdmin.logger.severe("Xml-Tag konnte nicht in Zahl umgewandelt werden: " + exc);
-    	}
-    	
-    	// Auslesen der dazugehörigen Schrift(en)
-    	if ( xmlElement.getFirstChildElement("schriften") != null ) {
-    		zugehoerigeSchrift = new IdLinkList(this);
-    		zugehoerigeSchrift.loadXmlElement(xmlElement.getFirstChildElement("schriften"));
-    	}
+    public CharKomponente getCharKomponente() {
+        return CharKomponente.sprache;
     }
-    
-    /* (non-Javadoc) Methode überschrieben
-     * @see org.d3s.alricg.charKomponenten.CharElement#writeXmlElement()
-     */
-    public Element writeXmlElement(){
-    	Element xmlElement = super.writeXmlElement();
 
-    	xmlElement.setLocalName("sprache");
-    	
-    	// Schreiben der Kostenklasse
-    	xmlElement.appendChild(new Element("nichtMuttersprache"));
-    	xmlElement.getFirstChildElement("nichtMuttersprache").addAttribute(
-    				new Attribute("kostenKlasse", kostenNichtMutterSpr.getXmlValue())
-    			);
-    
-    	// Schreiben der Komplexität
-    	xmlElement.getFirstChildElement("nichtMuttersprache").addAttribute(
-				new Attribute("komplexitaet", Integer.toString(komplexNichtMutterSpr))
-			);
-    	
-    	// Schreiben der dazugehörigen Schrift(en)
-    	if ( zugehoerigeSchrift != null ) {
-    		xmlElement.appendChild( zugehoerigeSchrift.writeXmlElement("schriften") );
-    	}
-    		
-    	return xmlElement;
+    /**
+     * Konstruktur; id beginnt mit "SPR-" für Sprache
+     * 
+     * @param id Systemweit eindeutige id
+     */
+    public Sprache(String id) {
+        setId(id);
+    }
+
+    /**
+     * @return Liefert das Attribut zugehoerigeSchrift.
+     */
+    public IdLinkList getZugehoerigeSchrift() {
+        return zugehoerigeSchrift;
+    }
+
+    /**
+     * @return Liefert das Attribut komplexNichtMutterSpr.
+     */
+    public int getKomplexNichtMutterSpr() {
+        return komplexNichtMutterSpr;
+    }
+
+    /**
+     * @return Liefert das Attribut kostenNichtMutterSpr.
+     */
+    public KostenKlasse getKostenNichtMutterSpr() {
+        return kostenNichtMutterSpr;
+    }
+
+    /**
+     * @param komplexNichtMutterSpr Setzt das Attribut komplexNichtMutterSpr.
+     */
+    public void setKomplexNichtMutterSpr(int komplexNichtMutterSpr) {
+        this.komplexNichtMutterSpr = komplexNichtMutterSpr;
+    }
+
+    /**
+     * @param kostenNichtMutterSpr Setzt das Attribut kostenNichtMutterSpr.
+     */
+    public void setKostenNichtMutterSpr(KostenKlasse kostenNichtMutterSpr) {
+        this.kostenNichtMutterSpr = kostenNichtMutterSpr;
+    }
+
+    /**
+     * @param zugehoerigeSchrift Setzt das Attribut zugehoerigeSchrift.
+     */
+    public void setZugehoerigeSchrift(IdLinkList zugehoerigeSchrift) {
+        this.zugehoerigeSchrift = zugehoerigeSchrift;
     }
 }
---
+++ alricg/src/org/d3s/alricg/store/xom/map/XOMMapper_Sprache.java
@@ -0,0 +1,70 @@
+package org.d3s.alricg.store.xom.map;
+
+import java.util.logging.Level;
+
+import nu.xom.Attribute;
+import nu.xom.Element;
+
+import org.d3s.alricg.charKomponenten.CharElement;
+import org.d3s.alricg.charKomponenten.Sprache;
+import org.d3s.alricg.charKomponenten.links.IdLinkList;
+import org.d3s.alricg.controller.ProgAdmin;
+import org.d3s.alricg.prozessor.FormelSammlung;
+
+class XOMMapper_Sprache extends XOMMapper_SchriftSprache implements XOMMapper {
+
+    public void map(Element xmlElement, CharElement charElement) {
+        super.map(xmlElement, charElement);
+
+        // my mapping
+        final Sprache sprache = (Sprache) charElement;
+
+        // Auslesen der Kostenklasse der Sprache
+        Element current = xmlElement.getFirstChildElement("nichtMuttersprache");
+        String kostenklasse = current.getAttributeValue("kostenKlasse");
+        sprache.setKostenNichtMutterSpr(FormelSammlung.getKostenKlasseByValue(kostenklasse));
+
+        // Auslesen der Kompläxität der Sprache
+        try {
+            sprache.setKomplexNichtMutterSpr(Integer.parseInt(current.getAttributeValue("komplexitaet")));
+        } catch (NumberFormatException exc) {
+            ProgAdmin.logger.log(Level.SEVERE, "String -> int failed", exc);
+        }
+
+        // Auslesen der dazugehörigen Schrift(en)
+        current = xmlElement.getFirstChildElement("schriften");
+        if (current != null) {
+            final IdLinkList zugehoerigeSchrift = new IdLinkList(sprache);
+            XOMMappingHelper.mapIdLinkList(current, zugehoerigeSchrift);
+            sprache.setZugehoerigeSchrift(zugehoerigeSchrift);
+        }
+    }
+
+    public void map(CharElement charElement, Element xmlElement) {
+        super.map(charElement, xmlElement);
+
+        // my mapping
+        final Sprache sprache = (Sprache) charElement;
+        xmlElement.setLocalName("sprache");
+
+        // Schreiben der Kostenklasse
+        Element e = new Element("nichtMuttersprache");
+        xmlElement.appendChild(e);
+        String kosten = sprache.getKostenNichtMutterSpr().getValue();
+        e.addAttribute(new Attribute("kostenKlasse", kosten));
+
+        // Schreiben der Komplexität
+        String komplexitaet = Integer.toString(sprache.getKomplexNichtMutterSpr());
+        e.addAttribute(new Attribute("komplexitaet", komplexitaet));
+
+        // Schreiben der dazugehörigen Schrift(en)
+        final IdLinkList zugehoerigeSchrift = sprache.getZugehoerigeSchrift();
+        if (zugehoerigeSchrift != null) {
+            e = new Element("schriften");
+            XOMMappingHelper.mapIdLinkList(zugehoerigeSchrift, e);
+            xmlElement.appendChild(e);
+        }
+
+    }
+
+}
