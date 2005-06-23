9c6f19ff3432e11ed66a9e4e1ba784da157d0db3
vstrelow <vstrelow>
2005-06-23 13:13:32 +0000

    Um SKT erweitert

--- alricg/src/org/d3s/alricg/prozessor/FormelSammlung.java
+++ alricg/src/org/d3s/alricg/prozessor/FormelSammlung.java
@@ -8,8 +8,11 @@
  */
 package org.d3s.alricg.prozessor;
 
+import java.util.HashMap;
+
 import org.d3s.alricg.charKomponenten.CharElement;
 import org.d3s.alricg.controller.ProgAdmin;
+import org.d3s.alricg.store.ConfigurationException;
 
 /**
  * <u>Beschreibung:</u><br> 
@@ -17,8 +20,11 @@ import org.d3s.alricg.controller.ProgAdmin;
  * @author V. Strelow
  */
 public class FormelSammlung {
-	public static int KEIN_WERT = CharElement.KEIN_WERT;
-	
+	public final static int KEIN_WERT = CharElement.KEIN_WERT;
+	private static HashMap<KostenKlasse, Integer[]> skt = new HashMap<KostenKlasse, Integer[]>();
+	//private static int[] sktPlus = { 50, 100, 150, 200, 250, 375, 500, 1000 }; // [sktSpalte] -> Der Wert bei über 30
+ 	private final static int maxSktStufe = 31;
+ 	
 	/**
 	 * Bildet die Spalten der SKT ab und bietet rudimentäre Operationen.
 	 *
@@ -47,10 +53,10 @@ public class FormelSammlung {
 		/**
 		 * Liefert eine KostenKlasse die um einen schritt "nach Rechts" geschoben
 		 * ist, also die KostenKlasse und somit die Kosten um eine Spalte erhöht.
-		 * 
-		 * @return Die um eine Spalte erhöhte Kostenklasse, bzw. das Maximum, 
 		 * @param kostenK Die bisherige Kostenklasse
-		 * 		wenn es nicht höher geht.
+		 * 		w
+		 * @return Die um eine Spalte erhöhte Kostenklasse, bzw. das Maximum, 
+		 *  wenn es nicht höher geht.
 		 */
 		public KostenKlasse plusEineKk() {
 			if ( this.ordinal() == (KostenKlasse.values().length - 1) ) {
@@ -83,6 +89,38 @@ public class FormelSammlung {
     	sehrGuterLehrmeister // ein Spalte leichter
     }
     
+    public static void initFormelSanmmlung() {
+    	try {
+    		skt = ProgAdmin.config.getSkt();
+    	} catch (ConfigurationException e) {
+    		// TODO Richtige Fehlermeldung einbauen!
+    		System.out.println("Konnte SKT nicht laden!");
+    	}
+    }
+    
+    /**
+     * Liefert einen Wert einer Zeile/Spalte der Steigerungskosten Tabelle.
+     * @param kKlasse Die gewünschte KostenKlasse
+     * @param stufe Die gewünschte Stufe
+     * @return Den Wert, der in der SKT in der Zeile "KostenKlasse" und in der 
+     * 		Spalte "Stufe". 
+     */
+    public static int getSktWert(KostenKlasse kKlasse, int stufe) {
+    	
+    	if (stufe > maxSktStufe) {
+    		stufe = maxSktStufe; // Ab 31 kosten alle Stufen gleich viel
+    	}
+    	
+    	if (kKlasse.equals(KostenKlasse.A_PLUS)) {
+    		int tmp = (skt.get(KostenKlasse.A)[stufe] - 2);
+    		if (tmp < 1) tmp = 1;
+    		return tmp;
+    	} else {
+    		return skt.get(kKlasse)[stufe];
+    	}
+    	
+    }
+    
 	/**
 	 * Liefert zu einem XML-Tag die entsprechende Enum zurück.
 	 * @param value Id  der KostenKlasse
@@ -108,17 +146,82 @@ public class FormelSammlung {
 	 * @param startStufe Die bisherige Stufe des Elements; KEIN_WERT oder ein negativer 
 	 * 			Wert bedeutet das es nicht aktiviert wurde!
 	 * @param zielStufe Die gewünschte Stufe
-	 * @param heldenStufe Die Stufe des Helden (nur bei aktivierung wichtig)
+	 * @param heldenStufe Die Stufe des Helden (nur bei aktivierung wichtig) 
+	 * 				 "0" Bedeutet: Generierung = Wert aus der Tabelle
+	 * 				 Negative Werte sind nicht erlaubt! 
 	 * @param methode Die gewünschte Lehrnmethode
 	 * @param klasse Die gewünschte Kostenklasse
+	 * @param isTalent Ob es sich um ein Talent handelt oder nicht.
+	 * 			(Talente besitzen kein A* und es gil: Selbststudium + Stufe > 10 = kk+1)
 	 * @return Die AP-Kosten für die Steigerung (inkl. evtl. aktivierung)
 	 */
 	public static int berechneSktKosten(int startStufe, int zielStufe, 
 								   int heldenStufe, Lernmethode methode, 
-								   KostenKlasse kKlasse) {
-		// TODO implement
+								   KostenKlasse kKlasse, boolean isTalent) {
+		KostenKlasse tmpKK = kKlasse;
+		int tmpKosten = 0;
 		
-		return 0;
+		// TODO Texte über library einlesen!
+		
+		// Änderung der Kostenklasse durch die Lernmethode
+		switch (methode) {
+			case selbstStudium: 
+				tmpKK = tmpKK.plusEineKk(); 
+				ProgAdmin.notepad.addSecondaryMsg("Selbststudium: K+1");
+				break;
+			case spezielleErfahrung:  
+				tmpKK = tmpKK.minusEineKk(); 
+				ProgAdmin.notepad.addSecondaryMsg("Spezielle Erfahrung: K-1");
+				break;
+			case sehrGuterLehrmeister:
+				tmpKK = tmpKK.minusEineKk(); 
+				ProgAdmin.notepad.addSecondaryMsg("Sehr guter Lehrmeister: K+1");
+				break;
+		}
+		
+		// Für Talente gibt es kein A+ als KostenKlasse
+		if ( tmpKK.equals(KostenKlasse.A_PLUS) && isTalent) {
+			tmpKK = KostenKlasse.A;
+		}
+		
+		// Falls es das Talent zuvor nicht gab, ist es KEIN_WERT, zählt aber wie "0"
+		if (startStufe == CharElement.KEIN_WERT) {
+			startStufe = -1;
+		}
+		
+		// Aktivierungskosten ausrechenen, wenn Stufe < 0
+		if (startStufe < 0) {
+			while (startStufe < 0 && startStufe < zielStufe) {
+				tmpKosten += getSktWert(tmpKK, heldenStufe);
+				startStufe++;
+			}
+		}
+		
+		// Unterscheiden, ob im Laufe der Berechnung die Kosten Kategorie steigt
+		if ( isTalent && zielStufe > 10 && methode.equals(Lernmethode.selbstStudium)) {
+			// Errechnen der Kosten / Kategorie ändert sich
+			while (startStufe < zielStufe) {
+				startStufe++;
+				
+				// Steigerung der Kategorie bei überschreiben der Stufe 10
+				if (startStufe == 11) {
+					tmpKK = tmpKK.plusEineKk();
+					ProgAdmin.notepad.addSecondaryMsg("Selbststudium + Talent + ab Stufe 11: K+1");
+				}
+
+				tmpKosten += getSktWert(tmpKK, startStufe);
+			}
+			
+		} else {
+		
+			// Errechnen der Kosten / Kategorie ändert sich nicht
+			while (startStufe < zielStufe) {
+				startStufe++;
+				tmpKosten += getSktWert(tmpKK, startStufe);
+			}
+		}
+		
+		return tmpKosten;
 	}
 	
 	/**
@@ -132,7 +235,20 @@ public class FormelSammlung {
 	 * @return Die AP-Kosten für die Steigerung (inkl. evtl. aktivierung)
 	 */
 	public static int berechneSktKosten(int startStufe, int zielStufe, KostenKlasse kKlasse) {
-		return berechneSktKosten(startStufe, zielStufe, 1, Lernmethode.lehrmeister, kKlasse);
+		return berechneSktKosten(startStufe, zielStufe, 1, 
+				Lernmethode.lehrmeister, kKlasse, false);
+		// Es wird Talent einfach angenommen, da es für die Lernmethode "Lehrmeister" egal ist
+	}
+	
+	/**
+	 * Berechnet die Kosten zum Senken der Schlechten Eigenschaften
+	 * @param startStufe
+	 * @param zielStufe
+	 * @return
+	 */
+	public static int berechneSchEigSenkenKosten(int startStufe, int zielStufe) {
+		// TODO implement
+		return 0;
 	}
 	
 	/**
