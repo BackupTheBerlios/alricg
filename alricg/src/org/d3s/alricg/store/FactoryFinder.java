e28dbe6c38d1803fbd681a6a4e6c04c1c8fd6938
msturzen <msturzen>
2005-10-09 12:04:32 +0000

    Reformat

--- alricg/src/org/d3s/alricg/store/FactoryFinder.java
+++ alricg/src/org/d3s/alricg/store/FactoryFinder.java
@@ -13,7 +13,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.lang.reflect.Constructor;
 import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
@@ -22,8 +21,9 @@ import java.util.logging.Logger;
 /**
  * Eine Finderklasse zur Auswahl der konkreten <code>AbstractStoreFactory</code>.
  * <p>
- * Der <code>FactoryFinder</code> wählt anhand einer Konfigurationsdatei aus, welche konkrete
- * <code>AbstractStoreFactory</code> verwendet werden soll. <br>
+ * Der <code>FactoryFinder</code> wählt anhand einer Konfigurationsdatei aus,
+ * welche konkrete <code>AbstractStoreFactory</code> verwendet werden soll.
+ * <br>
  * <h4>Verwendung</h4>
  * 
  * <pre>
@@ -34,108 +34,125 @@ import java.util.logging.Logger;
  * AbstractStoreFactory factorz = FactoryFinder.find(); // gibt die konkrete Factory zurück.
  * </pre>
  * 
- * Die <code>init</code>-Methode muss ausgeführt werden, bevor die <code>find</code>-Methode zum ersten Mal
- * aufgerufen wird.
+ * Die <code>init</code>-Methode muss ausgeführt werden, bevor die
+ * <code>find</code>-Methode zum ersten Mal aufgerufen wird.
  * <h4>Initialisierung</h4>
- * Der <code>FactoryFinder</code> sucht in der Datei <code>ressourcen/factory.properties</code>, in der angegebenen
+ * Der <code>FactoryFinder</code> sucht in der Datei
+ * <code>ressourcen/factory.properties</code>, in der angegebenen
  * Reihenfolge, nach folgenden Schlüsseln:
  * <ol>
  * <li>data.store.factory.impl</li>
  * <li>data.store.factory.default</li>
  * </ol>
- * Der Eintrag zum erstne gefundenen Schlüssel, wird als Klassenname interpretiert und versucht über Reflection zu
- * laden. Die Klasse muss das Interface {@link org.d3s.alricg.store.AbstractStoreFactory} implementieren.
+ * Der Eintrag zum erstne gefundenen Schlüssel, wird als Klassenname
+ * interpretiert und versucht über Reflection zu laden. Die Klasse muss das
+ * Interface {@link org.d3s.alricg.store.AbstractStoreFactory} implementieren.
  * </p>
  * 
  * @author <a href="mailto:msturzen@mac.com">St. Martin</a>
  */
 public final class FactoryFinder {
 
-    /** <code>FactoryFinder</code>'s logger */
-    private static final Logger LOG = Logger.getLogger(FactoryFinder.class.getName());
+	/** <code>FactoryFinder</code>'s logger */
+	private static final Logger LOG = Logger.getLogger(FactoryFinder.class
+			.getName());
 
-    /**
-     * Die zu verwendende <code>AbstractStoreFactory</code>.
-     */
-    private static AbstractStoreFactory factoryInstance;
+	/**
+	 * Die zu verwendende <code>AbstractStoreFactory</code>.
+	 */
+	private static AbstractStoreFactory factoryInstance;
 
-    /**
-     * Gibt die Factory zurück.
-     * 
-     * @return Die zu verwendende <code>AbstractStoreFactory</code>.
-     * @throws NullPointerException Falls <code>factoryInstance==null</code> ist.
-     */
-    public static final AbstractStoreFactory find() {
-        if (factoryInstance != null) {
-            return factoryInstance;
-        }
-        throw new NullPointerException("AbstractStoreFactory is not initialised!");
-    }
+	/**
+	 * Gibt die Factory zurück.
+	 * 
+	 * @return Die zu verwendende <code>AbstractStoreFactory</code>.
+	 * @throws NullPointerException
+	 *             Falls <code>factoryInstance==null</code> ist.
+	 */
+	public static final AbstractStoreFactory find() {
+		if (factoryInstance != null) {
+			return factoryInstance;
+		}
+		throw new NullPointerException(
+				"AbstractStoreFactory is not initialised!");
+	}
 
-    /**
-     * Initialisiert die zu verwendende <code>AbstractStoreFactory</code>, sofern das noch nicht geschehen ist und
-     * gibt sie zurück.
-     * 
-     * @param factoryFile Die Datei aus der die <code>AbstractStoreFactory</code> Implementierung gelesen werden soll.
-     * @return Die zu verwendende <code>AbstractStoreFactory</code>.
-     * @throws ConfigurationException Falls während der Initialisierung der Facotry ein Fehler auftritt.
-     */
-    public static final AbstractStoreFactory init(File factoryFile) throws ConfigurationException {
-        if (factoryInstance != null) {
-            return factoryInstance;
-        }
+	/**
+	 * Initialisiert die zu verwendende <code>AbstractStoreFactory</code>,
+	 * sofern das noch nicht geschehen ist und gibt sie zurück.
+	 * 
+	 * @param factoryFile
+	 *            Die Datei aus der die <code>AbstractStoreFactory</code>
+	 *            Implementierung gelesen werden soll.
+	 * @return Die zu verwendende <code>AbstractStoreFactory</code>.
+	 * @throws ConfigurationException
+	 *             Falls während der Initialisierung der Facotry ein Fehler
+	 *             auftritt.
+	 */
+	public static final AbstractStoreFactory init(File factoryFile)
+			throws ConfigurationException {
+		if (factoryInstance != null) {
+			return factoryInstance;
+		}
 
-        synchronized (FactoryFinder.class) {
-            try {
-                String classname = "org.d3s.alricg.store.xom.XOMFactory";
-                if (factoryFile.exists() && factoryFile.canRead()) {
-                    classname = getClassName(factoryFile);
-                }
-                final Class<?> clazz = Class.forName(classname);
-                final Constructor<?> conny = clazz.getConstructor(new Class[0]);
-                factoryInstance = (AbstractStoreFactory) conny.newInstance(new Object[0]);
-            } catch (Exception e) {
-                factoryInstance = null;
-                LOG.log(Level.SEVERE, "AbstractStoreFactory instantiation failed!", e);
-                throw new ConfigurationException("AbstractStoreFactory instantiation failed!", e);
-            }
-            factoryInstance.initialize();
-            return factoryInstance;
-        }
+		synchronized (FactoryFinder.class) {
+			try {
+				String classname = "org.d3s.alricg.store.xom.XOMFactory";
+				if (factoryFile.exists() && factoryFile.canRead()) {
+					classname = getClassName(factoryFile);
+				}
+				final Class< ? > clazz = Class.forName(classname);
+				factoryInstance = (AbstractStoreFactory) clazz.newInstance();
+			} catch (Exception e) {
+				factoryInstance = null;
+				LOG.log(Level.SEVERE,
+						"AbstractStoreFactory instantiation failed!", e);
+				throw new ConfigurationException(
+						"AbstractStoreFactory instantiation failed!", e);
+			}
+			factoryInstance.initialize();
+			return factoryInstance;
+		}
 
-    }
+	}
 
-    private static String getClassName(File factoryFile) throws FileNotFoundException, IOException {
-        ResourceBundle rb = new PropertyResourceBundle(new FileInputStream(factoryFile));
-        try {
-            return rb.getString("data.store.factory.impl");
-        } catch (NullPointerException npe) {
-            try {
-                return rb.getString("data.store.factory.default");
-            } catch (NullPointerException npe2) {
-                return "org.d3s.alricg.store.xom.XOMFactory";
-            }
-        }
-    }
+	private static String getClassName(File factoryFile)
+			throws FileNotFoundException, IOException {
+		ResourceBundle rb = new PropertyResourceBundle(new FileInputStream(
+				factoryFile));
+		try {
+			return rb.getString("data.store.factory.impl");
+		} catch (NullPointerException npe) {
+			try {
+				return rb.getString("data.store.factory.default");
+			} catch (NullPointerException npe2) {
+				return "org.d3s.alricg.store.xom.XOMFactory";
+			}
+		}
+	}
 
-    /**
-     * Initialisiert die zu verwendende <code>AbstractStoreFactory</code>, sofern das noch nicht geschehen ist und
-     * gibt sie zurück.
-     * 
-     * @see #init(File)
-     * @return Die zu verwendende <code>AbstractStoreFactory</code>.
-     * @throws ConfigurationException Falls während der Initialisierung der Facotry ein Fehler auftritt.
-     */
-    public static final AbstractStoreFactory init() throws ConfigurationException {
-        return init(new File("ressourcen/factory.properties"));
-    }
+	/**
+	 * Initialisiert die zu verwendende <code>AbstractStoreFactory</code>,
+	 * sofern das noch nicht geschehen ist und gibt sie zurück.
+	 * 
+	 * @see #init(File)
+	 * @return Die zu verwendende <code>AbstractStoreFactory</code>.
+	 * @throws ConfigurationException
+	 *             Falls während der Initialisierung der Facotry ein Fehler
+	 *             auftritt.
+	 */
+	public static final AbstractStoreFactory init()
+			throws ConfigurationException {
+		return init(new File("ressourcen/factory.properties"));
+	}
 
-    /**
-     * Setzt die zu verwendende <code>AbstractStoreFactory</code> auf <code>null</code>.
-     */
-    static final void reset() {
-        synchronized (FactoryFinder.class) {
-            factoryInstance = null;
-        }
-    }
+	/**
+	 * Setzt die zu verwendende <code>AbstractStoreFactory</code> auf
+	 * <code>null</code>.
+	 */
+	static final void reset() {
+		synchronized (FactoryFinder.class) {
+			factoryInstance = null;
+		}
+	}
 }
